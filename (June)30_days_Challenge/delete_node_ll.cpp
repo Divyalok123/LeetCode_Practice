4a78fe1f9ab01a7062870afe178ab5103a40f317
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-06-02 12:56:29 +0530

    some changes 2nd day

--- (June)30_days_Challenge/delete_node_ll.cpp
+++ (June)30_days_Challenge/delete_node_ll.cpp
@@ -13,8 +13,9 @@ struct ListNode {
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        node->val = node->next->val;
+        ListNode* temp = node->next;
+        node->val = temp->val;
         node->next = node->next->next;
-        return;
+        delete temp;
     }
 };
