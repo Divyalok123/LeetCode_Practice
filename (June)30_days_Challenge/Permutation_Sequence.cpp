8fd32ebefbcb3d922147dc6d19cadf7146b14d18
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-06-21 23:51:08 +0530

    21st day

--- (June)30_days_Challenge/Permutation_Sequence.cpp
+++ (June)30_days_Challenge/Permutation_Sequence.cpp
@@ -6,50 +6,56 @@
 using namespace std;
 
 /* ----------------- Solution 2 ----------------------- */
-class Solution {
+class Solution
+{
     int factof[10] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+
 public:
-    string getPermutation(int n, int k) {
-        
+    string getPermutation(int n, int k)
+    {
+
         string s, ans;
-        for(int i = 1; i <= n; i++)
+        for (int i = 1; i <= n; i++)
             s.push_back(i + '0');
 
-        while(s.size() != 0)
+        while (s.size() != 0)
         {
-            int x = ceil((float)k/factof[n-1]);
-            ans.push_back(s[x-1]);
-            s.erase(x-1, 1);
-            k -= (k/factof[n-1])*factof[n-1];
-            if(k == 0) {
+            int x = ceil((float)k / factof[n - 1]);
+            ans.push_back(s[x - 1]);
+            s.erase(x - 1, 1);
+            k -= (k / factof[n - 1]) * factof[n - 1];
+            if (k == 0)
+            {
                 reverse(s.begin(), s.end());
                 ans += s;
                 break;
             }
             n--;
         }
-        
+
         return ans;
     }
 };
 
 /* ----------------- Solution 1 (STL) ----------------- */
-class Solution {
+class Solution
+{
 public:
-    string getPermutation(int n, int k) {
+    string getPermutation(int n, int k)
+    {
         string s;
-        for(int i = 1; i <=n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            s.push_back(i+'0');
+            s.push_back(i + '0');
         }
-        
+
         sort(s.begin(), s.end());
-        
-        for(int i = 0; i < k-1; i++)
+
+        for (int i = 0; i < k - 1; i++)
         {
             next_permutation(s.begin(), s.end());
         }
-        
+
         return s;
-    }   
+    }
 };
---
+++ (June)30_days_Challenge/dungeon_game.cpp
@@ -0,0 +1,39 @@
+// The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon. The dungeon consists of M x N rooms laid out in a 2D grid. Our valiant knight (K) was initially positioned in the top-left room and must fight his way through the dungeon to rescue the princess.
+// The knight has an initial health point represented by a positive integer. If at any point his health point drops to 0 or below, he dies immediately.
+// Some of the rooms are guarded by demons, so the knight loses health (negative integers) upon entering these rooms; other rooms are either empty (0's) or contain magic orbs that increase the knight's health (positive integers).
+// In order to reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+class Solution
+{
+public:
+    int calculateMinimumHP(vector<vector<int>> &dungeon)
+    {
+        int n = dungeon.size();
+        int m = dungeon[0].size();
+
+        vector<vector<int>> answer(n, vector<int>(m));
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                if (i == n - 1 && j == m - 1)
+                    answer[i][j] = dungeon[i][j] < 0 ? 1 - dungeon[i][j] : 1;
+                else if (i == n - 1)
+                    answer[i][j] = max((answer[i][j + 1] - dungeon[i][j]), 1);
+                else if (j == m - 1)
+                    answer[i][j] = max((answer[i + 1][j] - dungeon[i][j]), 1);
+                else
+                    answer[i][j] = max(1, min(answer[i][j + 1], answer[i + 1][j]) - dungeon[i][j]);
+            }
+        }
+
+        return answer[0][0];
+    }
+};
