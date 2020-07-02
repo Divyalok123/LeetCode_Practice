99e2e486be0d692f1fe40b48f03084270bc2719c
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-07-03 04:24:50 +0530

    2nd day

---
+++ (June)30_days_Challenge/word_search2.cpp
@@ -0,0 +1,13 @@
+// Given a 2D board and a list of words from the dictionary, find all words in the board.
+// Each word must be constructed from letters of sequentially adjacent cell, 
+// where "adjacent" cells are those horizontally or vertically neighboring.
+// The same letter cell may not be used more than once in a word.
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+
