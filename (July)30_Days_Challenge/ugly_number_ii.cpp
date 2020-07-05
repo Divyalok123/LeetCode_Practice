cd147b1163ef81a1d3f45246f48c0a516168cb08
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-07-05 11:24:04 +0530

    updated ugly number problem

--- (July)30_Days_Challenge/ugly_number_ii.cpp
+++ (July)30_Days_Challenge/ugly_number_ii.cpp
@@ -6,10 +6,38 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cmath>
+
 using namespace std;
 
 /* --------- Solution 2 ----------- */
+class Solution
+{
+    vector<int> uglies;
+    bool flag = false;
+    int index_2 = 0, index_3 = 0, index_5 = 0;
+    
+public:
+    int nthUglyNumber(int n)
+    {
+        if(!flag) uglies.push_back(1);
+        while (uglies.size() < n)
+        {
 
+            int minim = min(uglies[index_2] * 2, min(uglies[index_3] * 3, uglies[index_5] * 5));
+
+            uglies.push_back(minim);
+
+            if (minim == uglies[index_2] * 2)
+                index_2++;
+            if (minim == uglies[index_3] * 3)
+                index_3++;
+            if (minim == uglies[index_5] * 5)
+                index_5++;
+        }
+        return uglies[n - 1];
+    }
+};
 
 /* --------- Solution 1 ----------- */
 class Solution
