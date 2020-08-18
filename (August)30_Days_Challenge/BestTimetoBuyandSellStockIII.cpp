72bd6705e8fc77254010e78d720533c27d6f7b57
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-08-19 02:26:20 +0530

    understood best time stock problem and added two more solutions

--- (August)30_Days_Challenge/BestTimetoBuyandSellStockIII.cpp
+++ (August)30_Days_Challenge/BestTimetoBuyandSellStockIII.cpp
@@ -7,6 +7,7 @@ https://leetcode.com/explore/featured/card/august-leetcoding-challenge/551/week-
 #include <algorithm>
 using namespace std;
 
+/*Solution 3*/
 class Solution
 {
 public:
@@ -28,5 +29,61 @@ public:
         }
 
         return dp[2];
+    }
+};
+
+/* Solition 2 */
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        
+        int** dp = new int*[3];
+        for(int i = 0; i < 3; i++)
+        {
+            dp[i] = new int[prices.size()];
+            for(int j = 0; j < prices.size(); j++)
+                dp[i][j] = 0;
+        }
+         
+        int minv[3];
+        for(int i = 0; i < 3; i++)
+            minv[i] = prices[0];
+        
+        for(int j = 1; j < prices.size(); j++) {
+            for(int i = 1; i <= 2; i++)
+            { 
+                minv[i] = min(minv[i], prices[j-1] - dp[i-1][j-1]);
+                dp[i][j] = max(dp[i][j-1], prices[j]-minv[i]);
+            }
+        }
+        return dp[2][prices.size()-1];
+    }
+};
+
+/* Solition 1 (TLE) */ 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        
+        int** dp = new int*[3], minv;
+        for(int i = 0; i < 3; i++)
+        {
+            dp[i] = new int[prices.size()];
+            for(int j = 0; j < prices.size(); j++)
+                dp[i][j] = 0;
+        }
+         
+        for(int j = 1; j < prices.size(); j++) {
+            for(int i = 1; i <= 2; i++)
+            { 
+                minv = prices[0];
+                for(int k = 1; k <= j; k++)
+                    minv = min(minv, prices[k] - dp[i-1][k-1]);
+                dp[i][j] = max(dp[i][j-1], prices[j]-minv);
+            }
+        }
+        return dp[2][prices.size()-1];
     }
 };
