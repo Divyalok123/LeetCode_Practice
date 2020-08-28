dd381f5ff6455807acfbfc59494704d00f14e798
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-08-28 11:45:21 +0530

    27th day

---
+++ (August)30_Days_Challenge/FindRightInterval.cpp
@@ -0,0 +1,31 @@
+/*
+https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3438/
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        map<int, int> m;
+        
+        int i;
+        for(i = 0; i < intervals.size(); i++)
+        {
+            m[intervals[i][0]] = i;
+        }
+        
+        vector<int> ans(intervals.size(), -1);
+        for(int j = 0; j < intervals.size(); j++)
+        {
+            auto pointer = m.lower_bound(intervals[j][1]);
+            if(pointer != m.end()) ans[j] = pointer->second;
+        }
+        
+        return ans;
+    }
+};
