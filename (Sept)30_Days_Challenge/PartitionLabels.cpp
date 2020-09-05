7eb613937a8d42233b876e0d2c27fd2f6338b396
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-09-05 07:41:10 +0530

    4th day

---
+++ (Sept)30_Days_Challenge/PartitionLabels.cpp
@@ -0,0 +1,29 @@
+/*
+https://leetcode.com/explore/featured/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3448/
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+        if(S.size() == 0) return {};
+        vector<int> v(26);
+        vector<int> ans;
+        int i;
+        for(i = 0; i < S.size(); i++)
+            v[S[i]-'a'] = i;
+        
+        int maxim = -1, last = 0;
+        for(i = 0; i < S.size(); i++)
+        {
+            maxim = max(maxim, v[S[i]-'a']);
+            if(maxim == i)
+                ans.push_back(maxim-last+1), last = i+1;
+        }
+        return ans;
+    }
+};
--- (Sept)30_Days_Challenge/RepeatedSubstringPattern.cpp
+++ (Sept)30_Days_Challenge/RepeatedSubstringPattern.cpp
@@ -7,44 +7,56 @@ https://leetcode.com/explore/featured/card/september-leetcoding-challenge/554/we
 #include <algorithm>
 using namespace std;
 
+//solution 3
+
+
 //solution 2: a really nice solution
-class Solution {
+class Solution
+{
 public:
-    
-    bool repeatedSubstringPattern(string& s) {
+    bool repeatedSubstringPattern(string &s)
+    {
         int n = s.size();
-        if(n <= 1) return false;
-        if(n == 2) return s[0] == s[1];
-                
-        string news = s+s;
-        news = news.substr(1, 2*n-2);
+        if (n <= 1)
+            return false;
+        if (n == 2)
+            return s[0] == s[1];
+
+        string news = s + s;
+        news = news.substr(1, 2 * n - 2);
         cout << news << endl;
         return news.find(s) != -1;
     }
 };
 
 //solution 1
-class Solution {
+class Solution
+{
 public:
-    bool check(string& s, int n1, string& ss, int n2) {
-        for(int i = 0; i < n1; i++)
+    bool check(string &s, int n1, string &ss, int n2)
+    {
+        for (int i = 0; i < n1; i++)
         {
-            if(s[i] != s[i%n2]) return false;
+            if (s[i] != s[i % n2])
+                return false;
         }
         return true;
     }
-    
-    bool repeatedSubstringPattern(string& s) {
+
+    bool repeatedSubstringPattern(string &s)
+    {
         int n = s.size();
-        if(n <= 1) return false;
-        if(n == 2) return s[0] == s[1];
-                
+        if (n <= 1)
+            return false;
+        if (n == 2)
+            return s[0] == s[1];
+
         string ss = "";
-        for(int i = 0; i < n/2; i++)
+        for (int i = 0; i < n / 2; i++)
         {
             ss += s[i];
-            if(n%(i+1)==0 && check(s, n, ss, i+1)) return true;
-            
+            if (n % (i + 1) == 0 && check(s, n, ss, i + 1))
+                return true;
         }
         return false;
     }
