c1d92add8e4c10628497d76ebc8cf8777439b31c
Divyalok Jaiswal <divyjais2001@gmail.com>
2020-09-12 08:53:23 +0530

    prev ques

--- (Sept)30_Days_Challenge/BullsandCows.cpp
+++ (Sept)30_Days_Challenge/BullsandCows.cpp
@@ -7,3 +7,26 @@ https://leetcode.com/explore/featured/card/september-leetcoding-challenge/555/we
 #include <algorithm>
 using namespace std;
 
+class Solution
+{
+public:
+    string getHint(string secret, string guess)
+    {
+        vector<int> v(10, 0);
+        for (auto &a : secret)
+            v[a - '0']++;
+
+        int cows = 0;
+        for (auto &a : guess)
+            if (v[a - '0'])
+                v[a - '0']--, cows++;
+
+        int bulls = 0;
+        for (int i = 0; i < secret.size(); i++)
+            if (secret[i] == guess[i])
+                bulls++;
+
+        cows -= bulls;
+        return to_string(bulls) + "A" + to_string(cows) + "B";
+    }
+};
