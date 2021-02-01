0b67d0a8b9a4107842329ca6c2fc0f7f24eb9db2
Divyalok Jaiswal <divyjais2001@gmail.com>
2021-02-01 13:57:35 +0530

    1st day

---
+++ (Feb)30_days_challenge_2021/Numberof1Bits.cpp
@@ -0,0 +1,27 @@
+/*
+https://leetcode.com/explore/featured/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3625/
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// Solution 2
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        for(int i = 31; i >= 0; i--) {
+            if(n&(1<<i)) ans++;
+        }
+        return ans;
+    }
+};
+
+// Solution 1 one-liner
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        return __builtin_popcount(n);
+    }
+};
